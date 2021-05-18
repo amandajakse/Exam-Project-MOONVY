06d197886b0c55b7db13d173bad794262608fa1b
KarolinaKrysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2021-05-18 17:53:32 +0200

    changing images

--- MOONVY_code/js/js.js
+++ MOONVY_code/js/js.js
@@ -1,1 +1,14 @@
 /** HERE WE WRITE JAVASCRIPT **/
+
+var image = document.getElementById("image");
+var images = ["pictures/edward-xu-eTVe9uwYV84-unsplash%20(1).png", "pictures/andie-gomez-acebo-sEq4onJnWrI-unsplash.png", "pictures/EstTnfPW4AE8Y_G.jpg"]
+var currentImage = 0;
+
+function changePhotoLoop() {
+	image.src = images[currentImage];
+
+	if (++currentImage >= images.length)
+		currentImage = 0;
+}
+
+setInterval(changePhotoLoop, 8000);
